e2665979d1b713bb196a9c30c1789e33663eee2f
saber_same <saber_same@foxmail.com>
2020-01-14 10:54:55 +0800

    init

---
+++ login/case_login.py
@@ -0,0 +1,56 @@
+from common.Base_Unit import Base_Base
+from public.Setting_Unit import Setting_Base as Set
+
+
+class Case_Login(Base_Base):
+
+    def __init__(self):
+        super().__init__()
+
+    # 清除用户名
+    def username_input_clear(self, case_name):
+        self.clear(case_name, "resource", "com.dfwd.wlkt:id/username_et")
+
+    # 输入用户名
+    def username_input_input(self, case_name, username):
+        self.input(case_name, "resource", "com.dfwd.wlkt:id/username_et", username)
+
+    # 清除密码
+    def passwd_input_clear(self, case_name):
+        self.clear(case_name, "resource", "com.dfwd.wlkt:id/password_et")
+
+    # 输入密码
+    def passwd_input_input(self, case_name, passwd):
+        self.input(case_name, "resource", "com.dfwd.wlkt:id/password_et", passwd)
+
+    # 点击登录按钮
+    def login_button_click(self, case_name):
+        self.click(case_name, "resource", "com.dfwd.wlkt:id/tv_login")
+
+    # 登陆成功断言
+    def toptitle_text_assertion(self, case_name, compare):
+        self.assertion(case_name, "resource", "com.dfwd.wlkt:id/tv_main_title_user", "你好，研发测试高中1")
+
+    # 登录失败断言
+    def errortitle_text_assertion(self, case_name, compare):
+        self.assertion(case_name, "resource", "com.dfwd.wlkt:id/title", "用户登录")
+
+    # 启动时日志记录
+    def start_log(self):
+        self.log(self.Set.history_log, "info", "正在执行登录脚本......")
+
+    # 结束时日志记录
+    def end_log(self):
+        self.log(self.Set.history_log, "info", "登陆脚本执行完毕.")
+
+    # 意外情况 - 电子笔校验
+    def accident_verify_button_click(self):
+        self.d.watcher.when('//*[@resource-id="com.dfwd.wlkt:id/tv_cancel"]').click()
+
+    # 开启意外处理
+    def watcher_start(self):
+        self.d.watcher.start()
+
+    # 结束意外处理
+    def watcher_close(self):
+        self.d.watcher.reset()
---
+++ login/test_login.py
@@ -0,0 +1,49 @@
+from case.case_login import Case_Login
+from public.Data_Unit import Data_Base
+import pytest
+
+
+class Test_Login:
+    # driver对象
+    d = None
+
+    def setup_class(self):
+        self.d = Case_Login()
+        self.d.start_log()
+        # 预料外的弹出框关闭
+        self.d.accident_verify_button_click()
+        self.d.watcher_start()
+
+    def teardown_class(self):
+        self.d.end_log()
+        # 清空监控
+        self.d.watcher_close()
+    
+    @pytest.mark.run(order=1)
+    @pytest.mark.parametrize(argnames="username", argvalues=data_list["e_username"])
+    @pytest.mark.parametrize(argnames="passwd", argvalues=data_list["e_passwd"])
+    def test_error_login(self, username, passwd, case_name="login_error"):
+
+        self.d.username_input_clear(case_name)
+        self.d.username_input_input(case_name, username)
+        self.d.passwd_input_clear(case_name)
+        self.d.passwd_input_input(case_name, passwd)
+        self.d.login_button_click(case_name)
+
+        # 断言
+        self.d.errortitle_text_assertion(case_name)
+
+    @pytest.mark.run(order=2)
+    @pytest.mark.parametrize(argnames="username", argvalues=data_list["c_username"])
+    @pytest.mark.parametrize(argnames="passwd", argvalues=data_list["c_passwd"])
+    def test_correct_login(self, username, passwd, case_name="login_correct"):
+
+        self.d.username_input_clear(case_name)
+        self.d.username_input_input(case_name, username)
+        self.d.passwd_input_clear(case_name)
+        self.d.passwd_input_input(case_name, passwd)
+        self.d.login_button_click(case_name)
+
+        # 断言
+        self.d.toptitle_text_assertion(case_name)
+
